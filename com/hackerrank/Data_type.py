ce579231d481bcf084508fd4f6eca6e5af5d7d6a
Shaina Ranjan <shaina@Keshavs-MacBook-Pro.local>
2021-08-02 23:18:50 -0700

    Data Type Program

---
+++ com/hackerrank/Data_type.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+    # Declare second integer, double, and String variables.
+    input_int = int(input())
+    input_double = float(input())
+    input_string = str(input())
+
+    print(i + input_int)
+    print(round(d + input_double, 1))
+    print(s + input_string)
+
+    # Read and save an integer, double, and String to your variables.
+
+    # Print the sum of both integer variables on a new line.
+
+    # Print the sum of the double variables on a new line.
+
+    # Concatenate and print the String variables on a new line
+    # The 's' variable above should be printed first.
+
+
+      
+
+
+
+
+
