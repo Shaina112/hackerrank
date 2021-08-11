83c16d647bf5ff6f356f881e999efd9e34d673d2
Shaina <shainagupta9253@gmail.com>
2021-08-10 23:14:19 -0700

    changes done

--- com/hackerrank/interfaces.py
+++ com/hackerrank/interfaces.py
@@ -6,7 +6,8 @@ if __name__ == '__main__':
 
 
     class Calculator(AdvancedArithmetic):
-        def divisorSum(self,n):
+        def divisorSum(self, n):
+            #this is a comment
             running_total = 0
             for i in range(1, n + 1):
                 if n % i == 0:
