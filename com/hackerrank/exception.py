9efa2953311f7d8c34b3846efcd282844f591a65
Shaina <shainagupta9253@gmail.com>
2021-09-02 09:54:37 -0700

    Added a test program with i

--- com/hackerrank/exception.py
+++ com/hackerrank/exception.py
@@ -1,8 +1,8 @@
-import math
-import os
-import random
-import re
-import sys
 
 if __name__ == '__main__':
+    i=0
     print("this is a test for exception")
+    i+=1
+    print("This statement executed")
+    i+=2
+    print("value of i = " + str(i))
