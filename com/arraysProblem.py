51bc76246d6d2d03ecc5da2e90c8898e15133d39
Shaina <shainagupta9253@gmail.com>
2022-04-21 14:22:52 -0700

    flipped the numbers

---
+++ com/arraysProblem.py
@@ -0,0 +1,11 @@
+import numpy
+
+def arrays(arr):
+    return numpy.flipud(numpy.array(arr,float))
+
+
+if __name__ == '__main__':
+
+    arr = input().strip().split(' ')
+    result = arrays(arr)
+    print(result)
